aa94025d5c7afc8a48524415d6118d2886bd267e
Roberto Franco <roberto.yuri.franco@gmail.com>
2022-11-09 22:25:30 -0300

    JS e web app

---
+++ html/js/main.js
@@ -0,0 +1,36 @@
+function bgBlue(){
+    document.getElementById("quadro").
+        style.backgroundColor="#00FF00";
+}
+
+function bgRed(){
+    document.getElementById("quadro").
+        style.backgroundColor="#FF0000";
+}
+document.getElementById("quadro")
+    .addEventListener("mouseenter", bgRed);
+console.log(document.getElementById("quadro"));
+
+for(var i = 0; i < 1; i++){
+    alert(i);
+}
+console.log(document.getElementById("quadro"));
+document.getElementById("quadro").
+    style.height="500px";
+document.getElementById("quadro").
+    style.width="500px";
+document.getElementById("quadro").
+    style.backgroundColor="#000000";
+
+const nome = "Roberto Franco";
+var variavel = 0;
+let v2 = 0;
+
+if(v2 > 5.9){
+    alert("Aprovado");
+}else{
+    alert("Reprovado");
+}
+
+
+
